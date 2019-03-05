60ed0d1a71a01530b12478f1125084ada81d4a19
gukbong2 <junyeon2459@gmail.com>
2019-03-05 12:36:50 +0900

    20190305/2

--- src/main/java/org/zerock/interceptor/SampleInterceptor.java
+++ src/main/java/org/zerock/interceptor/SampleInterceptor.java
@@ -1,7 +1,34 @@
 package org.zerock.interceptor;
 
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SampleInterceptor extends HandlerInterceptorAdapter{
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println();
+		System.out.println("===post handle===");
+		System.out.println();
+	}
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println();
+		System.out.println("===pre handle===");
+		System.out.println();
+		
+		HandlerMethod method = (HandlerMethod) handler;
+		Method methodObj = method.getMethod();
+		
+		System.out.println("Bean : " + method.getBean());
+		System.out.println("Method : " + methodObj);
+		return true;
+	}
 	
 }
