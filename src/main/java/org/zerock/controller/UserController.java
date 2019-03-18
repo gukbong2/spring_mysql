449feb661edb53a4652510acabf6e7e0bea98f6f
gukbong2 <junyeon2459@gmail.com>
2019-03-18 13:44:45 +0900

    20190318/2

--- src/main/java/org/zerock/controller/UserController.java
+++ src/main/java/org/zerock/controller/UserController.java
@@ -3,6 +3,9 @@ package org.zerock.controller;
 import java.util.Date;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.WebUtils;
 import org.zerock.domain.UserVO;
 import org.zerock.dto.LoginDTO;
 import org.zerock.service.UserService;
@@ -41,6 +45,30 @@ public class UserController {
 			
 			userService.keepLogin(vo.getUid(), session.getId(), sessionLimit);
 		}
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET) 
+	public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
+		Object obj = session.getAttribute("login");
+		
+		if (obj != null) {
+			UserVO vo = (UserVO) obj;
+			
+			session.removeAttribute("login");
+			session.invalidate();
+			
+			Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+			
+			if(loginCookie != null) {
+				loginCookie.setPath("/");
+				loginCookie.setMaxAge(0);
+				response.addCookie(loginCookie);
+				userService.keepLogin(vo.getUid(), session.getId(), new Date());
+			}
+		}
+		
+		return "user/logout";
+		
 	}
 	
 	
